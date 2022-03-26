896ec3ae1e3f6d0cc709512e59eb966a438ec327
Ellephy <aungphyopi@gmail.com>
2022-03-26 15:51:39 +0630

    Initial Commit

---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Aung Phyo Paing on 26/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
