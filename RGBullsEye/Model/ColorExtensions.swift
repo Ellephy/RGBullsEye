0c04860ebf451aa17568713ca7efee16bd930148
Ellephy <aungphyopi@gmail.com>
2022-03-27 18:04:33 +0630

    With Info.plist

--- RGBullsEye/Model/ColorExtensions.swift
+++ RGBullsEye/Model/ColorExtensions.swift
@@ -8,6 +8,9 @@
 import Foundation
 import SwiftUI
 
+/// Create a Color view from an RGB object.
+///   - parameters:
+///     - rgb: The RGB object.
 extension Color {
     init(rgbStruct rgb: RGB) {
         self.init(red: rgb.red, green: rgb.green, blue: rgb.blue)
